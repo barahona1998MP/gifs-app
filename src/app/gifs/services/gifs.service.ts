a88a36aa3488505fe6e00fd88aba3c21e3d14361
barahona1998MP <marcosplata98@gmail.com>
2024-06-03 21:28:46 -0500

    validate the component search-box

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,9 +12,20 @@ export class GifsService {
     return [...this._tagHistory];
   }
 
-  searchTag(tag:string):void {
+  private validateHistory(tag:string):void {
+    tag = tag.toLowerCase();
+    if(this._tagHistory.includes(tag)) {
+      this._tagHistory = this._tagHistory.filter(oldTag => oldTag !== tag);
+    }
+
     this._tagHistory.unshift(tag);
 
+    this._tagHistory = this._tagHistory.splice(0, 10);
+  }
+
+  searchTag(tag:string):void {
+    if(tag.length === 0) return
+    this.validateHistory(tag);
     console.log(this._tagHistory)
   }
 }
