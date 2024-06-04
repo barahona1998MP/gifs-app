a4e815f1c94bfc0ed8ca07754700d58ab34d9b80
barahona1998MP <marcosplata98@gmail.com>
2024-06-04 09:25:16 -0500

    refactor components of gifs

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { ListGifsComponent } from './components/list-gifs/list-gifs.component';
-
+import { CardComponent } from './components/card/card.component';
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
     SearchBoxComponent,
-    ListGifsComponent
+    ListGifsComponent,
+    CardComponent,
   ],
   imports: [
     CommonModule,
